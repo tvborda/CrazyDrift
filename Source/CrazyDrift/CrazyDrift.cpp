8d1bfe158a76f7063f3e7d3adfb39aa450036c5f
Thiago Borda <tvborda@gmail.com>
2017-01-27 02:30:39 -0500

    Add project files

---
+++ Source/CrazyDrift/CrazyDrift.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CrazyDrift.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrazyDrift, "CrazyDrift" );
